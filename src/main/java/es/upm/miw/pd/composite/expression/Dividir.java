3e3d322715ebed7207643c99b5d193f36648b062
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-13 21:37:39 +0200

    composite pattern: created Dividir class

---
+++ src/main/java/es/upm/miw/pd/composite/expression/Dividir.java
@@ -0,0 +1,23 @@
+package es.upm.miw.pd.composite.expression;
+
+public class Dividir extends Expresion {
+
+    private Expresion exp1;
+
+    private Expresion exp2;
+
+    public Dividir(Expresion exp1, Expresion exp2) {
+        this.exp1 = exp1;
+        this.exp2 = exp2;
+    }
+
+    @Override
+    public int operar() {
+        return exp1.operar() / exp2.operar();
+    }
+
+    @Override
+    public String toString() {
+        return "(" + exp1.toString() + "/" + exp2.toString() + ")";
+    }
+}
