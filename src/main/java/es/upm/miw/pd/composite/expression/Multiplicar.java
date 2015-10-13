598b6d624626084d49d5a02fa0ca9020061a315e
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-13 22:11:58 +0200

    composite pattern: updated Multiplicar class

--- src/main/java/es/upm/miw/pd/composite/expression/Multiplicar.java
+++ src/main/java/es/upm/miw/pd/composite/expression/Multiplicar.java
@@ -1,14 +1,9 @@
 package es.upm.miw.pd.composite.expression;
 
-public class Multiplicar extends Expresion  {
-
-    private Expresion exp1;
-
-    private Expresion exp2;
+public class Multiplicar extends Operacion {
 
     public Multiplicar(Expresion exp1, Expresion exp2) {
-        this.exp1 = exp1;
-        this.exp2 = exp2;
+        super(exp1, exp2);
     }
 
     @Override
@@ -17,7 +12,7 @@ public class Multiplicar extends Expresion  {
     }
 
     @Override
-    public String toString() {
-        return "(" + exp1.toString() + "*" + exp2.toString() + ")";
+    protected String operacionString() {
+        return "*";
     }
 }
