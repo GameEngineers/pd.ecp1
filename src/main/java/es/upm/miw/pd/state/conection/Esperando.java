be0d52036c94fb518c603e3ea833e7bd5c873061
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-15 20:53:07 +0200

    Add Esperando

---
+++ src/main/java/es/upm/miw/pd/state/conection/Esperando.java
@@ -0,0 +1,17 @@
+package es.upm.miw.pd.state.conection;
+
+public class Esperando extends EstadoAbstracto {
+
+    public Esperando() {
+        this.estado = Estado.ESPERANDO;
+    }
+
+    @Override
+    public void recibir(Conexion conexion, int respuesta) {
+        if (respuesta == 0) {
+            conexion.setEstado(new Preparado());
+        } else if (respuesta > 0) {
+            conexion.setEstado(new Cerrado());
+        }
+    }
+}
