ff41e2e56ae1d770e4c5664855d53904b7cea23c
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-15 20:53:45 +0200

    Add Preparado

---
+++ src/main/java/es/upm/miw/pd/state/conection/Preparado.java
@@ -0,0 +1,36 @@
+package es.upm.miw.pd.state.conection;
+
+public class Preparado extends EstadoAbstracto {
+
+    public Preparado(){
+        this.estado = Estado.PREPARADO;
+    }
+ 
+    @Override
+    public void iniciar(Conexion conexion) {
+        conexion.setEstado(new Preparado());
+    }
+
+    @Override
+    public void abrir(Conexion conexion) {
+        conexion.setEstado(new Preparado());
+    }
+
+    @Override
+    public void cerrar(Conexion conexion) {
+        conexion.setEstado(new Cerrado());
+    }
+
+    @Override
+    public void enviar(Conexion conexion, String msg) {
+        conexion.getLink().enviar(msg);
+        conexion.setEstado(new Esperando());
+        
+    }
+    
+    @Override
+    public void parar(Conexion conexion) {
+        conexion.setEstado(new Parado());
+    }
+
+}
