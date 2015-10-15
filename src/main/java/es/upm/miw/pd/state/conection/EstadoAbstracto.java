19f45371d2e15f02d3cc317dc3dbbad56c4e1016
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-15 20:52:30 +0200

    Add EstadoAbstracto

---
+++ src/main/java/es/upm/miw/pd/state/conection/EstadoAbstracto.java
@@ -0,0 +1,38 @@
+package es.upm.miw.pd.state.conection;
+
+public abstract class EstadoAbstracto {
+
+    protected Estado estado;
+
+    public void abrir(Conexion conexion) {
+        this.lanzarOperacionNoPermitida();
+    }
+
+    public void cerrar(Conexion conexion) {
+        this.lanzarOperacionNoPermitida();
+    }
+
+    public void parar(Conexion conexion) {
+        this.lanzarOperacionNoPermitida();
+    }
+
+    public void iniciar(Conexion conexion) {
+        this.lanzarOperacionNoPermitida();
+    }
+
+    public void enviar(Conexion conexion, String msg) {
+        this.lanzarOperacionNoPermitida();
+    }
+
+    public void recibir(Conexion conexion, int respuesta) {
+        this.lanzarOperacionNoPermitida();
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    private void lanzarOperacionNoPermitida() {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+    }
+}
