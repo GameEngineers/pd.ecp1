edd0e53e7d80fc1cd4259c1addc2958f55180044
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-15 20:52:51 +0200

    Add Cerrado

---
+++ src/main/java/es/upm/miw/pd/state/conection/Cerrado.java
@@ -0,0 +1,18 @@
+package es.upm.miw.pd.state.conection;
+
+public class Cerrado extends EstadoAbstracto {
+
+    public Cerrado(){
+        this.estado = Estado.CERRADO;
+    }
+   
+    @Override
+    public void cerrar(Conexion conexion) {
+        conexion.setEstado(new Cerrado());
+    }
+
+    @Override
+    public void abrir(Conexion conexion) {
+        conexion.setEstado(new Preparado());
+    }
+}
